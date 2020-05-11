ed434ac1c828b95278f21a500eb9c9a96b772d7d
erjiale <jiale98chinoguay@gmail.com>
2020-05-11 19:00:10 -0400

    added js and more mobile responsive design

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function scrollToAbout() {
+    var elem = document.getElementById('about-sect')
+    elem.scrollIntoView({behavior:"smooth"});
+}
+
+function scrollToProjects() {
+    var elem = document.getElementById('projects-sect')
+    elem.scrollIntoView({behavior:"smooth"});
+}
