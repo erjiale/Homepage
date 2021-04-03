5f8ad69a899a273ca180caf7ead293d21a477d77
Jiale <jiale98chinoguay@gmail.com>
2021-04-03 18:43:50 -0400

    [Update] - Setup Sass config - migrate CSS to Sass

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Modals
+    function projectModals() {
+        var modal = document.getElementById('recipeFinder-modal');
+        var openDiv = document.getElementById('openRecipeFinderModal');
+        var span = document.getElementsByClassName('close')[0];
+
+        openDiv.onclick = function () {
+            modal.style.display = "block";
+        }
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+    projectModals();
+})
+
 // Scrolling Into View
 function scrollToHome() {
     var elem = document.getElementById('home-sect')
-    elem.scrollIntoView({behavior:"smooth"});
+    elem.scrollIntoView({ behavior: "smooth" });
 }
 function scrollToAbout() {
     var elem = document.getElementById('about-sect')
-    elem.scrollIntoView({behavior:"smooth"});
+    elem.scrollIntoView({ behavior: "smooth" });
 }
 function scrollToProjects() {
     var elem = document.getElementById('projects-sect')
-    elem.scrollIntoView({behavior:"smooth"});
+    elem.scrollIntoView({ behavior: "smooth" });
 }
 
-// Modals
-window.onload = function() {
-    var modal = document.getElementById('recipeFinder-modal');
-    var openDiv = document.getElementById('openRecipeFinderModal');
-    var span = document.getElementsByClassName('close')[0];
-    
-    openDiv.onclick = function() {
-        modal.style.display = "block";
-    }
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-    }
-}
