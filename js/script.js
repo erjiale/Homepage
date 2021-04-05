811419c326d3b2065f88f8f8f368fbe80ef7738f
Jiale <jiale98chinoguay@gmail.com>
2021-04-04 22:37:50 -0400

    [Update] - Changed Landing section UI, Header UI, and Header Burger menu

--- js/script.js
+++ js/script.js
@@ -23,15 +23,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Scrolling Into View
 function scrollToHome() {
+    console.log("home")
     var elem = document.getElementById('home-sect')
+    var menu_elem = document.getElementById("navbar");
+    var checkBox = document.getElementById("checkBtn");
+    menu_elem.classList.remove("active-menu");
     elem.scrollIntoView({ behavior: "smooth" });
+    checkBox.classList.remove("checkedBox");
 }
 function scrollToAbout() {
     var elem = document.getElementById('about-sect')
+    var menu_elem = document.getElementById("navbar");
+    var checkBox = document.getElementById("checkBtn");
+    menu_elem.classList.remove("active-menu");
     elem.scrollIntoView({ behavior: "smooth" });
+    checkBox.classList.remove("checkedBox");
 }
 function scrollToProjects() {
     var elem = document.getElementById('projects-sect')
+    var menu_elem = document.getElementById("navbar");
+    var checkBox = document.getElementById("checkBtn");
+    menu_elem.classList.remove("active-menu");
     elem.scrollIntoView({ behavior: "smooth" });
+    checkBox.classList.remove("checkedBox");
 }
 
+// Header features
+const changeBackground = () => {
+    var navbar_elem = document.getElementById('navbar');
+    window.scrollY >= 80 ? navbar_elem.classList.add('active') : navbar_elem.classList.remove('active');
+};
+window.addEventListener('scroll', changeBackground);
+
+// Header burger menu
+function onToggleMenu() {
+    var checkBox = document.getElementById("checkBtn");
+    var check = document.getElementById("check");
+    var navbar_element = document.getElementById("navbar");
+    if (!check.checked) {
+        checkBox.classList.add("checkedBox");
+        navbar_element.classList.add("active-menu", "active");
+    }
+    else {
+        checkBox.classList.remove("checkedBox");
+        if (window.scrollY >= 80) {
+            navbar_element.classList.remove("active-menu");
+        }
+        else {
+            navbar_element.classList.remove("active-menu", "active");
+        }
+    }
+}
