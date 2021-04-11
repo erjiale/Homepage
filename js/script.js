f6a0672186459a0e5fa01a36272ffcc6fb6e68e2
Jiale <jiale98chinoguay@gmail.com>
2021-04-11 17:29:03 -0400

    [Update]: Change landing and navbar looks + add _projects.scss

--- js/script.js
+++ js/script.js
@@ -23,27 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Scrolling Into View
 function scrollToHome() {
-    console.log("home")
     var elem = document.getElementById('home-sect')
     var menu_elem = document.getElementById("navbar");
-    var checkBox = document.getElementById("checkBtn");
-    menu_elem.classList.remove("active-menu");
+    var checkBox = document.getElementById("check-button");
+    menu_elem.classList.remove("open-toggle-menu");
     elem.scrollIntoView({ behavior: "smooth" });
     checkBox.classList.remove("checkedBox");
 }
 function scrollToAbout() {
     var elem = document.getElementById('about-sect')
     var menu_elem = document.getElementById("navbar");
-    var checkBox = document.getElementById("checkBtn");
-    menu_elem.classList.remove("active-menu");
+    var checkBox = document.getElementById("check-button");
+    menu_elem.classList.remove("open-toggle-menu");
     elem.scrollIntoView({ behavior: "smooth" });
     checkBox.classList.remove("checkedBox");
 }
 function scrollToProjects() {
     var elem = document.getElementById('projects-sect')
     var menu_elem = document.getElementById("navbar");
-    var checkBox = document.getElementById("checkBtn");
-    menu_elem.classList.remove("active-menu");
+    var checkBox = document.getElementById("check-button");
+    menu_elem.classList.remove("open-toggle-menu");
     elem.scrollIntoView({ behavior: "smooth" });
     checkBox.classList.remove("checkedBox");
 }
@@ -51,26 +50,33 @@ function scrollToProjects() {
 // Header features
 const changeBackground = () => {
     var navbar_elem = document.getElementById('navbar');
-    window.scrollY >= 80 ? navbar_elem.classList.add('active') : navbar_elem.classList.remove('active');
+    window.scrollY >= 80 ? navbar_elem.classList.add('white-header') : navbar_elem.classList.remove('white-header');
 };
 window.addEventListener('scroll', changeBackground);
 
+
+
+////// IF WHITE HEADER, then black BURGER, else white BURGER
+//////
 // Header burger menu
 function onToggleMenu() {
-    var checkBox = document.getElementById("checkBtn");
+    var checkBox = document.getElementById("check-button");
     var check = document.getElementById("check");
     var navbar_element = document.getElementById("navbar");
+    var burger_menu = document.getElementById("menu-btn_burger");
     if (!check.checked) {
         checkBox.classList.add("checkedBox");
-        navbar_element.classList.add("active-menu", "active");
+        navbar_element.classList.add("open-toggle-menu");
     }
     else {
         checkBox.classList.remove("checkedBox");
         if (window.scrollY >= 80) {
-            navbar_element.classList.remove("active-menu");
+            navbar_element.classList.remove("open-toggle-menu");
+            // burger_menu.classList.remove("white-header");
         }
         else {
-            navbar_element.classList.remove("active-menu", "active");
+            navbar_element.classList.remove("open-toggle-menu");
+            // burger_menu.classList.remove("white-header");
         }
     }
 }
